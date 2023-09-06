76d141c7237520cc6b5947f3718418bdf403012e
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-07 02:56:35 +0600

    9. Relational Operators in Java

---
+++ RelationalOperators.java
@@ -0,0 +1,20 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 6;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        a = 56;
+        b = 87;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+}
