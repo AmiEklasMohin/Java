3a308f6635ee64c4d9970c7d4935261e5e962bbd
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-07 01:42:19 +0600

    4. Implicit TypeCasting (Widening) or Automatic TypeCasting

--- TypeConversion.java
+++ TypeConversion.java
@@ -1,14 +1,10 @@
-import java.util.Scanner;
-
 public class TypeConversion {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int a = 657;
         long b = a;
         System.out.println(b);
         char ch = 'a';
         int asciiCode = ch;
         System.out.println(asciiCode);
-        sc.close();
     }
 }
