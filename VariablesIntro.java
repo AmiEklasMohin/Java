4881892c841d1a630cee01bc3e0bd7abc911cc58
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-07 11:12:31 +0600

    2. Introduction to Variables

---
+++ VariablesIntro.java
@@ -0,0 +1,16 @@
+public class VariablesIntro {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println('a');
+        System.out.println(a);
+        System.out.println(b);
+        a = 50;
+        b = 112;
+        System.out.println(a);
+        a = b;
+        System.out.println(a);
+        String name = "Eklas Mohin";
+        System.out.println(name);
+    }
+}
