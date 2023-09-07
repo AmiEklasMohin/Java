19e7143eed9074378060a7b3267e292d4897aa86
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-07 10:56:53 +0600

    7. Types of Operators in Java

--- OperatorsInJava.java
+++ OperatorsInJava.java
@@ -2,11 +2,10 @@ public class OperatorsInJava {
     public static void main(String[] args) {
         System.out.println("There are Five Types of Operators in Java : ");
         String[] OperatorsInJava = {"Arithmetic Operators (Binary/Unary)",
-                                    "Relational Operators",
-                                    "Logical Operators",
-                                    "Bitwise Operators",
-                                    "Assignment Operators"
-                                   };
+                                   "Relational Operators",
+                                   "Logical Operators",
+                                   "Bitwise Operators",
+                                   "Assignment Operators"};
         for (int i = 0; i < OperatorsInJava.length; i++) {
             System.out.println((i + 1) + ". " + OperatorsInJava[i]);
         }
