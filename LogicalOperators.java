d13b80f7d1d8f939bacd7c12dd03e589c13cfe31
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-08 16:01:12 +0600

    10. Logical Operators in Java

---
+++ LogicalOperators.java
@@ -0,0 +1,14 @@
+public class LogicalOperators{
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        System.out.println(a > 2 && b > a);
+        System.out.println(a > 2 && b < a);
+        System.out.println(a < 2 && b < a);
+        System.out.println(a > 2 || b > a);
+        System.out.println(a > 2 || b < a);
+        System.out.println(a < 2 || b < a);
+        System.out.println(!(a > b));
+        System.out.println(!(a < b));
+        System.out.println(!(a == b));
+    }
+}
