df32de7f2c8793a4e4ad6e5eb510dd9bcbb56fe9
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-08 16:26:50 +0600

    12. Conditional Statements example (if...else)

---
+++ LargestOfTwo.java
@@ -0,0 +1,11 @@
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 5;
+        if (A >= B) {
+            System.out.println("A is the largest");
+        } else {
+            System.out.println("B is the largest");
+        }
+    }
+}
