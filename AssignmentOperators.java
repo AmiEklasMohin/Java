4745d751e24359f5a9a4c59636bd3bcc7c4cab3b
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-08 16:14:55 +0600

    11. Assignment Operators in Java

---
+++ AssignmentOperators.java
@@ -0,0 +1,20 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        a = a + 10;
+        System.out.println(a);
+        a += 10;
+        System.out.println(a);
+        int b = 78;
+        b -= 8;
+        System.out.println(b);
+        int k = 6;
+        k *= k;
+        System.out.println(k);
+        k /= 6;
+        System.out.println(k);
+        int x = 78;
+        x %= 7;
+        System.out.println(x);
+    }
+}
