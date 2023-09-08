ba57906ca02f8f11d2fae486799507e60649009f
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-08 18:27:14 +0600

    3. Primitive Data Types in Java

--- DataTypes.java
+++ DataTypes.java
@@ -4,17 +4,17 @@ public class DataTypes {
         System.out.println(b);
         short s = 240;
         System.out.println(s);
-        char ch = 'a';
-        System.out.println(ch);
-        boolean bool = true;
-        System.out.println(bool);
-        float cgpa = 3.27f;
-        System.out.println(cgpa);
-        int rollNumber = 25;
-        System.out.println(rollNumber);
-        long ll = 1574957473998567576L;
-        System.out.println(ll);
+        int i = 25;
+        System.out.println(i);
+        long l = 1574957473998567576L;
+        System.out.println(l);
+        float f = 3.27f;
+        System.out.println(f);
         double d = 26735946734902.924769576;
         System.out.println(d);
+        char c = 'a';
+        System.out.println(c);
+        boolean bool = true;
+        System.out.println(bool);
     }
 }
