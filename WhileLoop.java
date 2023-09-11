53908ad0cf58c55b5a4d9046e4832a7f2c0e6d15
Eklasur Rahman Mohin <143247354+AmiEklasMohin@users.noreply.github.com>
2023-09-11 15:50:23 +0600

    Delete WhileLoop.java

--- WhileLoop.java
+++
@@ -1,9 +0,0 @@
-public class WhileLoop {
-    public static void main(String[] args) {
-        int i = 1;
-        while (i <= 100) {
-            System.out.println("Hi, I'm Learning Java!");
-            i += 1;
-        }
-    }
-}
