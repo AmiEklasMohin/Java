85d53c6733b3cd48eaeee5243159bb3c68264bc5
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-11 21:55:47 +0600

    4. Implicit type casting or widening

---
+++ ImplicitTypeCasting.java
@@ -0,0 +1,10 @@
+public class ImplicitTypeCasting {
+    public static void main(String[] args) {
+        int a = 657;
+        long b = a;
+        System.out.println(b);
+        char ch = 'a';
+        int asciiCode = ch;
+        System.out.println(asciiCode);
+    }
+}
