b453c9e1565e5009b3a55335e5020c81777bd640
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-11 22:47:53 +0600

    8. Arithmetic operators in Java

--- ArithmeticOperators.java
+++ ArithmeticOperators.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class ArithmeticOperators {
     public static void main(String[] args) {
-        int A = 10;
-        int B = 5;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter A : ");
+        int A = scanner.nextInt();
+        System.out.print("Enter B : ");
+        int B = scanner.nextInt();
         System.out.println("Addition = A + B = " + (A + B));
         System.out.println("Subtraction = A - B = " + (A - B));
         System.out.println("Multiplication = A * B = " + (A * B));
@@ -15,5 +20,6 @@ public class ArithmeticOperators {
         System.out.println("A = " + A);
         System.out.println("--B = " + (--B));
         System.out.println("B = " + B);
+        scanner.close();
     }
 }
