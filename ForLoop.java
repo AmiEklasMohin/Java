7288b52a1a0406b54114a0a4835d51ea4533c16b
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-11 15:05:07 +0600

    16. Loops in Java (for loop)

---
+++ ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hi, I'm Learning Java!");
+        }
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
