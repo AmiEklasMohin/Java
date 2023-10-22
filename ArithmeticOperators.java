54ddb36bf212b8250d5d911b5b50d174fbde4f46
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-22 17:43:28 +0600

    8. Arithmetic operators in Java

--- ArithmeticOperators.java
+++ ArithmeticOperators.java
@@ -7,11 +7,11 @@ public class ArithmeticOperators {
         int A = scanner.nextInt();
         System.out.print("Enter B : ");
         int B = scanner.nextInt();
-        System.out.println("Addition = A + B = " + (A + B));
-        System.out.println("Subtraction = A - B = " + (A - B));
-        System.out.println("Multiplication = A * B = " + (A * B));
-        System.out.println("Division = A / B = " + (A / B));
-        System.out.println("Modulo = A % B = " + (A % B));
+        System.out.println("Addition => A + B = " + (A + B));
+        System.out.println("Subtraction => A - B = " + (A - B));
+        System.out.println("Multiplication => A * B = " + (A * B));
+        System.out.println("Division => A / B = " + (A / B));
+        System.out.println("Modulo => A % B = " + (A % B));
         System.out.println("A++ = " + (A++));
         System.out.println("A = " + A);
         System.out.println("++B = " + (++B));
