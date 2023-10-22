adf795af508f3d3cbeff2a152154ccd9f07c23ab
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-22 17:30:35 +0600

    19. Methods in Java

--- MethodsDemo.java
+++ MethodsDemo.java
@@ -4,7 +4,6 @@ class MethodsDemo {
     public static void printHelloWorld() {
         System.out.println("Hello World!");
     }
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
