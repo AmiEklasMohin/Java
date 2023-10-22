e245bb486555e93f3eeb59fdb23d6b9392fbb41f
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-22 19:20:05 +0600

    20. Example of method with parameters

--- SumOfTwo.java
+++ SumOfTwo.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class SumOfTwo {
-    public static int calculateSum(int numOne, int numTwo) { /*parameters or formal parameters*/
+    public static int calculateSum(int numOne, int numTwo) { /* parameters or formal parameters */
         int sum = numOne + numTwo;
         return  sum;
     }
@@ -9,7 +9,7 @@ class SumOfTwo {
         Scanner scan = new Scanner(System.in);
         int numOne = scan.nextInt();
         int numTwo = scan.nextInt();
-        int sum = calculateSum(numOne, numTwo); /*arguments or actual parameters*/
+        int sum = calculateSum(numOne, numTwo); /* arguments or actual parameters */
         System.out.println(sum);
         scan.close();
     }
