b11bfb4503a5ea79b0715674c88b27c815d38677
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-22 14:51:10 +0600

    7. Taking input from user

--- TakingUserInput.java
+++ TakingUserInput.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class TakingUserInput {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter your name : ");
-        String usersName = scanner.nextLine();
+        String usersName = scan.nextLine();
         System.out.print("Enter your age : ");
-        int usersAge = scanner.nextInt();
+        int usersAge = scan.nextInt();
         System.out.println("Name : " + usersName);
         System.out.println("Age : " + usersAge);
-        scanner.close();
+        scan.close();
     }
 }
