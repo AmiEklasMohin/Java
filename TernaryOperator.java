6102bcc408c9ebc0739ec79aee660e5fad029c44
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-23 11:22:00 +0600

    Ternary operator in Java

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class TernaryOperator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter an integer : ");
-        int firstNumber = scanner.nextInt();
+        int firstNumber = scan.nextInt();
         System.out.print("Enter another integer : ");
-        int secondNumber = scanner.nextInt();
+        int secondNumber = scan.nextInt();
         boolean flag = firstNumber == secondNumber ? true : false;
         if (flag == true) {
             System.out.println("Both are equal.");
@@ -14,6 +14,6 @@ public class TernaryOperator {
             int largestNumber = firstNumber  > secondNumber ? firstNumber  : secondNumber;
             System.out.println("Largest number = " + largestNumber);
         }
-        scanner.close();
+        scan.close();
     }
 }
