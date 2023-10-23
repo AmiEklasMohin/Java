8c00b06467617a447d06af153a5e2ab1545b4e79
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-23 11:46:45 +0600

    14. Example of nested if...else

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 
 public class LargestOfThree {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter A : ");
-        int A = scanner.nextInt();
+        int A = scan.nextInt();
         System.out.print("Enter B : ");
-        int B = scanner.nextInt();
+        int B = scan.nextInt();
         System.out.print("Enter C : ");
-        int C = scanner.nextInt();
+        int C = scan.nextInt();
         if (A >= B) {
             if (A >= C) {
-                System.out.println(A + " is the largest.");
+                System.out.println(A + " is the largest");
             } else {
-                System.out.println(C + " is the largest.");
+                System.out.println(C + " is the largest");
             }
         } else if (B >= A) {
             if (B >= C) {
-                System.out.println(B + " is the largest.");
+                System.out.println(B + " is the largest");
             } else {
-                System.out.println(C + " is the largest.");
+                System.out.println(C + " is the largest");
             }
         } else {
-            System.out.println(C + " is the largest.");
+            System.out.println(C + " is the largest");
         }
-        scanner.close();
+        scan.close();
     }
 }
