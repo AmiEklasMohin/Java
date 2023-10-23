af8a82d56430f9e34b80e9f2186b06d6412da41a
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-23 11:56:44 +0600

    18. Check a number is prime or composite using for loop

--- PrimeOrComposite.java
+++ PrimeOrComposite.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class PrimeOrComposite {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter a positive integer : ");
-        int anIntegerNumber = scanner.nextInt();
+        int anIntegerNumber = scan.nextInt();
         boolean isPrime = true;
         for (int i = 2; i <= (anIntegerNumber / 2); i++) {
             if (anIntegerNumber % i == 0) {
@@ -13,12 +13,12 @@ public class PrimeOrComposite {
             }
         }
         if (anIntegerNumber == 1) {
-            System.out.println("The number " + anIntegerNumber + " is neither a prime nor a composite.");
-        } else if (isPrime == true) {
-            System.out.println("The number " + anIntegerNumber + " is prime.");
+            System.out.println("The number " + anIntegerNumber + " is neither prime nor composite");
+        } else if (isPrime) {
+            System.out.println("The number " + anIntegerNumber + " is prime");
         } else {
-            System.out.println("The number " + anIntegerNumber + " is composite.");
+            System.out.println("The number " + anIntegerNumber + " is composite");
         }
-        scanner.close();
+        scan.close();
     }
 }
