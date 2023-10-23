ce6fbafd514a15bddfdfae332478ad66df6ddaa9
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-23 11:19:20 +0600

    6. Type promotion in expression

--- TypePromotionInExpression.java
+++ TypePromotionInExpression.java
@@ -4,17 +4,17 @@ public class TypePromotionInExpression {
         char b = 'b';
         System.out.println((int) (a));
         System.out.println((int) (b));
-        System.out.println(b - a);
+        System.out.println(b - a); /* promoted to int */
         System.out.println(a);
         System.out.println(b);
         int x = 10;
         float y = 20.25f;
         long z = 25;
         double d = 30.45;
-        double total = x + y + z + d;
+        double total = x + y + z + d; /* promoted to double */
         System.out.println(total);
-        byte bb = 5;
-        bb = (byte) (bb * 2);
-        System.out.println(bb);
+        byte bt = 5;
+        bt = (byte) (bt * 2); /* type casted into byte from int */
+        System.out.println(bt);
     }
 }
