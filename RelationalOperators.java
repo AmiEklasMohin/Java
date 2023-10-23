1f40b2b8458dab80211e981ec33cf1ba80a90686
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-23 11:52:41 +0600

    9. Relational operators in Java

--- RelationalOperators.java
+++ RelationalOperators.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class RelationalOperators {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    /*  Relational operators take two operands
+        compare their values
+        and return a Boolean value (true or false)  */
+        Scanner scan = new Scanner(System.in);
         System.out.print("A = ");
-        int A = scanner.nextInt();
+        int A = scan.nextInt();
         System.out.print("B = ");
-        int B = scanner.nextInt();
+        int B = scan.nextInt();
         System.out.println("A is equal to B -> " + (A == B));
         System.out.println("A is not equal to B -> " + (A != B));
         System.out.println("A is greater than B -> " + (A > B));
         System.out.println("A is less than B -> " + (A < B));
         System.out.println("A is greater than equal to B -> " + (A >= B));
         System.out.println("A is less than equal to B -> " + (A <= B));
-        scanner.close();
+        scan.close();
     }
 }
