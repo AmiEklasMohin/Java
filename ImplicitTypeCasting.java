85714fe2c80e3ae06d6bb3f129550a1e00da2525
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-23 11:43:44 +0600

    4. Implicit type casting or widening

--- ImplicitTypeCasting.java
+++ ImplicitTypeCasting.java
@@ -1,10 +1,10 @@
 public class ImplicitTypeCasting {
     public static void main(String[] args) {
         int a = 657;
-        long b = a;
+        long b = a; /* Automatically type casted into long */
         System.out.println(b);
         char ch = 'a';
-        int numericValue = ch;
-        System.out.println(numericValue);
+        int numericValue = ch; /* ASCII value of 'a' */
+        System.out.println(numericValue); 
     }
 }
