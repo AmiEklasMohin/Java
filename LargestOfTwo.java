3ea4c5ed3b4cdd9831aed4c29a1ec2511bfe8a16
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-23 12:01:08 +0600

    12. Conditional statement if...else

--- LargestOfTwo.java
+++ LargestOfTwo.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class LargestOfTwo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter A : ");
-        int A = scanner.nextInt();
+        int A = scan.nextInt();
         System.out.print("Enter B : ");
-        int B = scanner.nextInt();
+        int B = scan.nextInt();
         if (A >= B) {
-            System.out.println(A + " is the largest.");
+            System.out.println(A + " is the largest");
         } else {
-            System.out.println(B + " is the largest.");
+            System.out.println(B + " is the largest");
         }
-        scanner.close();
+        scan.close();
     }
 }
